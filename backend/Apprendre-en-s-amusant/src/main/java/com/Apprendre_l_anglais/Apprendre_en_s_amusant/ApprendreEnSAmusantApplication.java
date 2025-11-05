2c9cde6085c89b2151653296ff47dacd78490543
TheEternalLearner <michael.lanselle@protonmail.com>
2025-11-05 23:32:27 +0100

    backend added, gitignore updated

---
+++ backend/Apprendre-en-s-amusant/src/main/java/com/Apprendre_l_anglais/Apprendre_en_s_amusant/ApprendreEnSAmusantApplication.java
@@ -0,0 +1,13 @@
+package com.Apprendre_l_anglais.Apprendre_en_s_amusant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprendreEnSAmusantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprendreEnSAmusantApplication.class, args);
+	}
+
+}
---
+++ backend/Apprendre-en-s-amusant/src/test/java/com/Apprendre_l_anglais/Apprendre_en_s_amusant/ApprendreEnSAmusantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Apprendre_l_anglais.Apprendre_en_s_amusant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprendreEnSAmusantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
