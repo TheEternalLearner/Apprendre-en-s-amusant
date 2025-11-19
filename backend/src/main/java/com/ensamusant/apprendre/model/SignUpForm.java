7ed75a5df9319b84f2d5d5bf11bdc7d59f9c77c2
TheEternalLearner <michael.lanselle@protonmail.com>
2025-11-19 18:19:10 +0100

    Added @NotBlank to SignUpForm model, test is green

--- backend/src/main/java/com/ensamusant/apprendre/model/SignUpForm.java
+++ backend/src/main/java/com/ensamusant/apprendre/model/SignUpForm.java
@@ -1,11 +1,13 @@
 package com.ensamusant.apprendre.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class SignUpForm {
+    @NotBlank
     private String firstName;
     private String lastName;
     private String email;
