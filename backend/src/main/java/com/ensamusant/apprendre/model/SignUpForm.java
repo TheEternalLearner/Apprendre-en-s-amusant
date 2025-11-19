e45b9b644271a581e2782954f3c3ab603dc7b196
TheEternalLearner <michael.lanselle@protonmail.com>
2025-11-19 18:31:42 +0100

    whenEmailIsInvalid_thenValidationFails is green

--- backend/src/main/java/com/ensamusant/apprendre/model/SignUpForm.java
+++ backend/src/main/java/com/ensamusant/apprendre/model/SignUpForm.java
@@ -1,5 +1,6 @@
 package com.ensamusant.apprendre.model;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,5 +15,6 @@ public class SignUpForm {
     private String lastName;
 
     @NotBlank
+    @Email
     private String email;
 }
