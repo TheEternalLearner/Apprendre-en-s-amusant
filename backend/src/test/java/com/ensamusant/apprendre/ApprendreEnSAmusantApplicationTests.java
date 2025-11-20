6da1010117d2abb6231de4557f69cd7c05161456
TheEternalLearner <michael.lanselle@protonmail.com>
2025-11-20 20:13:20 +0100

    added application-test.properties and @ActiveProfile('test')

--- backend/src/test/java/com/ensamusant/apprendre/ApprendreEnSAmusantApplicationTests.java
+++ backend/src/test/java/com/ensamusant/apprendre/ApprendreEnSAmusantApplicationTests.java
@@ -2,8 +2,10 @@ package com.ensamusant.apprendre;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApprendreEnSAmusantApplicationTests {
 
 	@Test
