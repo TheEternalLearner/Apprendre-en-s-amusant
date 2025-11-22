265542a589c6ccd09d21b11144697cd7aa9caec1
TheEternalLearner <michael.lanselle@protonmail.com>
2025-11-22 16:33:15 +0100

    createCourse_ShouldReturnStatus200AndCreateCourse() test is red

--- backend/src/test/java/com/ensamusant/apprendre/integration/controller/CourseControllerIT.java
+++ backend/src/test/java/com/ensamusant/apprendre/integration/controller/CourseControllerIT.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -25,6 +27,25 @@ public class CourseControllerIT {
 
     @Autowired
     private CourseRepository courseRepository;
+
+    @Test
+    public void createCourse_ShouldReturnStatus200AndCreateCourse() throws Exception {
+        // Arrange
+        String formJson = "{\"title\":\"title\",\"description\":\"description\",\"imageUrl\":\"image.jpeg\",\"capacity\": 4,\"level\":\"beginner\",\"ageBracket\":\"6-8\"}";
+
+        // Act & Assert
+        mockMvc.perform(post("/api/courses")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(formJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value("title"))
+                .andExpect(jsonPath("$.description"). value("description"))
+                .andExpect(jsonPath("$.imageUrl").value("image.jpeg"))
+                .andExpect(jsonPath("$.capacity").value(4))
+                .andExpect(jsonPath("$.level").value("beginner"))
+                .andExpect(jsonPath("$.ageBracket").value("6-8"))
+                .andExpect(jsonPath("$.id").exists());
+    }
 
     @Test
     public void getAllCourses_ShouldReturnStatus200AndEmptyArray() throws Exception {
