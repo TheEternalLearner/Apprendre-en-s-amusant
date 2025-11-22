761a1f147baaf6525ac87e7fc9cfa5b12747a70c
TheEternalLearner <michael.lanselle@protonmail.com>
2025-11-22 16:50:04 +0100

    test is green

--- backend/src/main/java/com/ensamusant/apprendre/controller/CourseController.java
+++ backend/src/main/java/com/ensamusant/apprendre/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.ensamusant.apprendre.controller;
 import com.ensamusant.apprendre.model.Course;
 import com.ensamusant.apprendre.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +18,12 @@ public class CourseController {
 
     public CourseController(CourseService courseService) {
         this.courseService = courseService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Course> createCourse(@RequestBody Course newCourse) {
+        Course savedCourse = courseService.saveCourse(newCourse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCourse);
     }
 
     @GetMapping("")
