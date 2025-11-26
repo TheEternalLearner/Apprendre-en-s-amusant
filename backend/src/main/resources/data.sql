1006be5d5808005b40fe44d25dc7578396c5faa7
TheEternalLearner <michael.lanselle@protonmail.com>
2025-11-26 17:21:02 +0100

    added datasql, configuration in application.propoerties to initialize table

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO courses (title, capacity, level, day_of_week, time_slot, location) 
+VALUES ('Anglais Débutant 6-9 ans', 12, 'Débutant', 'Lundi', '14:00-16:00', 'Salle A');
+
+INSERT INTO courses (title, capacity, level, day_of_week, time_slot, location) 
+VALUES ('Anglais Intermédiaire 10-12 ans', 10, 'Intermédiaire', 'Mercredi', '10:00-12:00', 'Visio');
+
+INSERT INTO courses (title, capacity, level, day_of_week, time_slot, location) 
+VALUES ('Anglais Avancé Adultes', 8, 'Avancé', 'Vendredi', '16:00-18:00', 'La Sentinelle');
+
+INSERT INTO courses (title, capacity, level, day_of_week, time_slot, location) 
+VALUES ('Espagnol Débutant', 15, 'Débutant', 'Mardi', '18:00-20:00', 'Salle B');
+
+INSERT INTO courses (title, capacity, level, day_of_week, time_slot, location) 
+VALUES ('Allemand Intermédiaire', 10, 'Intermédiaire', 'Jeudi', '14:00-16:00', 'Visio');
