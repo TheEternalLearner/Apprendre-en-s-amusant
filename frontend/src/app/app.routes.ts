0ed80d3756d14105ef5b3dfb83b25f5eb446f472
TheEternalLearner <michael.lanselle@protonmail.com>
2025-11-26 19:29:27 +0100

    added new admin courses for admin panel

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './home/home';
 import { About } from './about/about';
 import { CourseSignUpComponent } from './course-sign-up/course-sign-up.component';
 import { CourseListComponent } from './course-list/course-list.component';
+import { CourseListComponent as AdminCourseListComponent } from './admin/course-list/course-list.component';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'A-propos', component: About},
     {path: 'inscription', component: CourseSignUpComponent},
-    {path: 'cours', component: CourseListComponent}
+    {path: 'cours', component: CourseListComponent},
+    {path: 'admin/cours', component: AdminCourseListComponent}
 ];
