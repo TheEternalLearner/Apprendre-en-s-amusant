672730b51fc38a49aa8f94627a9e3bf435171800
TheEternalLearner <michael.lanselle@protonmail.com>
2025-11-27 18:19:45 +0100

    fixed test CourseListComponent

--- frontend/src/app/course-list/course-list.component.spec.ts
+++ frontend/src/app/course-list/course-list.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { CourseListComponent } from './course-list.component';
 
@@ -8,7 +9,8 @@ describe('CourseListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CourseListComponent]
+      imports: [CourseListComponent],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
 
