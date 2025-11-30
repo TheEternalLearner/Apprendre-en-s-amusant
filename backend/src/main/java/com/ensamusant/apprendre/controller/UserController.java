bc384f0ef1fd40e9d61aaa48bd95fc273e47c4a3
TheEternalLearner <michael.lanselle@protonmail.com>
2025-11-30 21:35:06 +0100

    getAllUser_ShouldReturnStatus200AndEmptyArray() is green

--- backend/src/main/java/com/ensamusant/apprendre/controller/UserController.java
+++ backend/src/main/java/com/ensamusant/apprendre/controller/UserController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -27,5 +24,10 @@ public class UserController {
         newUser.setRole(Role.USER);
         User savedUser = userService.saveUser(newUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @GetMapping("")
+    public Iterable <User> getUsers() {
+        return userService.getUsers();
     }
 }
