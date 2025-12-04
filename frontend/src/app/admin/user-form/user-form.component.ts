2be8bffdf914e0ab5f41fd9a53cfe6fb72a83d8e
TheEternalLearner <michael.lanselle@protonmail.com>
2025-12-04 12:22:59 +0100

    Adding ngForm for user creation

--- frontend/src/app/admin/user-form/user-form.component.ts
+++ frontend/src/app/admin/user-form/user-form.component.ts
@@ -1,15 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { FormsModule, NgForm } from "@angular/forms";
+import { User } from '../../models/user.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
   private userService = inject(UserService);
-  onSubmit() {
+  private snackBar = inject(MatSnackBar);
+  private router = inject(Router);
 
+  user !: User;
+
+  onSubmit(form:NgForm) {
+    if(form.invalid) {
+      this.snackBar.open('Veuillez remplir tous les champs correctement.', 'OK', {
+        duration: 3000,
+        panelClass: ['snack-error']
+      });
+      return;
+    }
+    this.router.navigate("/admin/utilisateurs");
   }
 }
