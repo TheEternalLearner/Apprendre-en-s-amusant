26d461b5da322515cf115088c602191fb768fc26
TheEternalLearner <44651875+TheEternalLearner@users.noreply.github.com>
2026-01-19 13:21:45 +0100

    modified the angular.json to apply environment when needed and changed apiURl in services

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService { 
-  private apiUrl = 'http://localhost:8080/api/users'; 
+  private apiUrl = `${environment.apiUrl}/api/users`; 
   http = inject(HttpClient);
   
   createUser(user: User): Observable <User> {
